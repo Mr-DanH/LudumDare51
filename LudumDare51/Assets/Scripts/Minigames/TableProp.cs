2a4949187dd3b89084cb484e90046b0f16a9fefd
Dan <mrdanjh@gmail.com>
2022-10-01 21:31:56 +0100

    Add some gravity to throwable props

--- LudumDare51/Assets/Scripts/Minigames/TableProp.cs
+++ LudumDare51/Assets/Scripts/Minigames/TableProp.cs
@@ -3,15 +3,24 @@ using UnityEngine.EventSystems;
 
 public class TableProp : MinigameProp, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
+    public RectTransform m_table;
+
+    public float m_gravity = 9.8f;
+
     Vector3 m_velocity;
-    int m_dragFrame;
-    bool m_dragging;
-    bool m_thrown;
+    bool m_falling;
 
     public override void ResetState()
     {
+        if(gameObject.activeSelf)
+            return;
+
         base.ResetState();
-        m_thrown = false;
+
+        Canvas canvas = GetComponentInParent<Canvas>();
+        transform.position += Vector3.up * canvas.pixelRect.height;
+        m_falling = true;
+        m_velocity = Vector3.zero;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -28,27 +37,37 @@ public class TableProp : MinigameProp, IPointerDownHandler, IDragHandler, IPoint
         if(eventData.delta != Vector2.zero)
         {
             m_velocity = eventData.delta / Time.deltaTime;
-            m_thrown = true;
         }
         else
         {
-
+            m_velocity = Vector3.zero;
         }
+
+        m_falling = true;
     }
 
     void Update()
     {
-        if (m_thrown)
-        {
-            transform.position += m_velocity * Time.deltaTime;
-
-            Canvas canvas = GetComponentInParent<Canvas>();
-
+        if (m_falling)
+        {       
             RectTransform rectTransform = (RectTransform)transform;
             Rect rect = rectTransform.rect;            
             rect.position += (Vector2)rectTransform.position;
 
-            if(!rect.Overlaps(canvas.pixelRect))
+            Rect tableRect = m_table.rect;            
+            tableRect.position += (Vector2)m_table.position;
+
+            if(rect.Overlaps(tableRect))
+            {
+                m_falling = false;
+                return;
+            }
+
+            m_velocity.y -= m_gravity * Time.deltaTime;
+            transform.position += m_velocity * Time.deltaTime;
+            
+            Canvas canvas = GetComponentInParent<Canvas>();
+            if(rect.yMax < canvas.pixelRect.yMin || rect.xMax < canvas.pixelRect.xMin || rect.xMin > canvas.pixelRect.xMax)
                 gameObject.SetActive(false);
         }
     }
