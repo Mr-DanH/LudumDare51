6ce458e3e018a63637a0830fdee7435467c44f95
Dan <mrdanjh@gmail.com>
2022-10-02 18:19:00 +0100

    Hook up results

--- LudumDare51/Assets/Scripts/UI/MatchResultsScreen.cs
+++ LudumDare51/Assets/Scripts/UI/MatchResultsScreen.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MatchResultsScreen : SingletonMonoBehaviour<MatchResultsScreen>
 {
+    public Text m_descriptionText;
+    public Transform m_matchParent;
+
     ScreenSwoosh m_screenSwoosh;
 
     public override void Awake()
@@ -20,6 +24,31 @@ public class MatchResultsScreen : SingletonMonoBehaviour<MatchResultsScreen>
     public void Show()
     {
         gameObject.SetActive(true);
+
+        int numMatches = 0;
+
+        AlienManager.Instance.ForEachAlien(
+            delegate(OngoingAlienData alien)
+            {
+                if(alien.AlienRequestedMatch && alien.PlayerRequestedMatch)
+                {
+                    Transform matchEntry = m_matchParent.GetChild(numMatches);
+                    matchEntry.gameObject.SetActive(true);
+
+                    matchEntry.GetComponentInChildren<Text>().text = alien.Data.Name;
+                    ++numMatches;
+                }
+            });
+
+        for(int i = numMatches; i < m_matchParent.childCount; ++i)
+            m_matchParent.GetChild(i).gameObject.SetActive(false);
+
+        if(numMatches == 1)
+            m_descriptionText.text = "Congratulations, you got a match, could this be true love?";
+        else if(numMatches > 1)
+            m_descriptionText.text = "Congratulations, you got several matches, time to clear your schedule!";
+        else
+            m_descriptionText.text = "Unfortunately you didn't have any matches this time. Better luck next time!";
 
         m_screenSwoosh.AnimateOn();
     }
